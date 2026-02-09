d83c8624261c63adbdc05b324ec7b45bc9e57feb
Hanuel0119 <kej050119@gmail.com>
2026-02-09 15:38:25 +0900

    web-dev-2026

--- 03-javascript/practice07-lotto.js
+++ 03-javascript/practice07-lotto.js
@@ -1,26 +1,54 @@
-// 가로로 숫자 7개가 뜨는게 아니라 세로로 숫자 7개가 뜨는 거 수정중입니다! indexof가 문제인 것 같아서 찾아보는중입니다
-   
-let first = [];
-let second = [];
-let third = [];
-let fourth = [];
-let fifth = [];
+let numbers = [];
+
+while (numbers.length < 7) {
+    const random = Math.floor(Math.random() * 45) + 1;
+    if (!numbers.includes(random)) {
+    numbers.push(random);
+    }
+const bonus = numbers[numbers.length - 1];
+numbers = numbers.slice(0, 6);
+console.log(`numbers : ${numbers}, bonus : ${bonus}`);
+
+let counter = 0;
 
 while (true) {
-// 45까지 숫자 중 랜덤 숫자 뽑기
-var result = 0;
-
-for (var i= 0; i < 7; i++) {
-    var numbers = Math.floor(Math.random() * 45) + 1;
+const my = [];
+while (my.legth < 6) {
+        if ( my.includes(5, 15, 25, 35, 45, 3)) {
+        break;
+        }
+    }
+    console.log(my);
+}
 }
 
-// 배열 안에서 같은 코드가 있는지 찾고 무효 처리 하기
-if (result.indexOf(numbers) !== 1) {
-    i--; // i--는 이번 회차 무효 / 없던걸로 (기억해두기)
-} else {
-// 중복이 아니라면 다시 돌리기
-    result.push(numbers);
+console.log(`로또 번호 : ${lotto}`);
+console.log(`내 번호 : ${my}`);
+
+let count = 0;
+for(let num of lotto) {
+    if (my.includes(num)) {
+        count++;
+    }
+}
+console.log(`일치 개수 : ${count}`);
+if(count === 6) {
+    // 1등 당첨
+    console.log(`1등 당첨 ${counter}번 만에 맞추셨네요`)
+    break;
+} elso if(count === 5 && my.includes(bonus)) {
+    // 2등 당첨
+    console.log(`2등 당첨 ${counter}번 만에 맞추셨네요`)
+    break;
+} elso if (count === 4) {
+    // 3등 당첨
+    console.log(`3등 당첨 ${counter}번 만에 맞추셨네요`)
+    break;
 }
 
-console.log(result);
-}
+
+
+
+
+
+
