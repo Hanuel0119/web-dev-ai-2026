e76cabee36c61e0a4b9a0ff815dd0ae1299dbe4a
Hanuel0119 <kej050119@gmail.com>
2026-02-09 18:01:39 +0900

    web-dev-2026

--- 03-javascript/practice08-dom.js
+++ 03-javascript/practice08-dom.js
@@ -25,26 +25,64 @@ const minute = String(now.getMinutes()).padStart(2, "0");
 const weeks = ["일", "월", "화", "수", "목", "금", "토"];
 const week = weeks[now.getDay()];
 
-const colors = [
-  "#1abc9c",
-  "#3498db",
-  "#9b59b6",
-  "#e67e22",
-  "#e74c3c",
-]
-
+// 한국어, 영어
 const enEl = document.getElementById("en")
 const koEl = document.getElementById("ko")
 
-document.body.style.background = colors
+// 색 바꾸기
+ document.body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 
-const timer = setInterval(() => {
-    console.log("")
+setInterval(() => {
+    document.body.style.backgroundColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }, 3000);
 
+// 현재 시간
+const current = () => {
+    const now = new Date();
+    console.log(now.getFullYear()); // 년
+    console.log(now.getMonth() + 1); // 월
+    console.log(now.getDate()); // 일
+    console.log(now.weeks[now.getDay()]); // 요일
+    console.log(now.getHours()); // 시
+    console.log(now.getMinutes()); // 분
+    console.log(now.String(now.getSeconds()).padStart(2, "0")); // 초
+}
+
+// 남은 시간
+const countEl = document.getElementById("ko2");
+
+const count = () => {
+    const last = new Date("2026-02-09 16:45:00")
+    const now = new Date();
+    // console.log(Math.floor((last - now) / 1000));
+    const second = Math.floor((last - now) / 1000); // 1000ms = 1초
+    const minute = Math.floor(second / 60); // 60초 = 1분
+    const hour = Math.floor(minute / 60); // 60분 = 1시간
+    const day = Math.floor(hour / 24);// 24시간 = 1일
+    console.log(day); // 일
+    console.log(hour % 24); // 시간
+    console.log(minute % 60); // 분
+    console.log(second % 60); // 초
+
+    countEl.textContent = `올해 남은 시간: ${day}일 ${displayHours}시 ${displayMinutes}분 ${displaySeconds}초`;
+};
+
+
+const quoteEl = document.getElementById("quote");
+
+const randomQuote = () => {
+        const quote = quotes[Math.floor(Math.random() * quotes.length)];
+       enEl.textContent = quote.en;
+       koEl.textContent = quote.ko;
+       setInterval(randomQuote, 3000);
+    };
 
 
 
+randomQuote();
+current();
+count();
+setInterval(count, 1000);
 
 
 
